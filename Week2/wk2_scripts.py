2035095940bee27efb906cc8f75f61b8ab61b91e
Rob S <41172477+rszym88@users.noreply.github.com>
2020-04-28 15:05:21 -0400

    Week 2 scripts file

---
+++ Week2/wk2_scripts.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 14:58:37 2020
+Week 2 Scripts:
+    ACJ Inmate CSV Info Processing
+    Display Reverse Icon Solution
+    ACJ Inmate Count Text File Log
+@author: rms51
+"""
+"""
+HW:
+    Finish therotation effects for the displaz icon script
+    Scale the image up by 2, 5, 10, & plus odd number
+"""
+
